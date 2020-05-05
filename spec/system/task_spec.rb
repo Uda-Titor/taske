b28ebd8e286e6f0bcf88292f2541a632a6993097
ryohei udagawa <r.udagawa1995@gmail.com>
2020-05-05 17:22:39 +0900

    複数のタスクテスト追加

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -5,13 +5,22 @@ RSpec.describe 'タスク管理機能', type: :system do
 
   describe 'タスク一覧画面' do
     before do
-      @task = FactoryBot.create(:task, task_name: 'task')
+      @task = FactoryBot.create(:task)
     end
     context 'タスクを作成した場合' do
       it '作成済みのタスクが表示される' do
         FactoryBot.create(:task)
         visit tasks_path
         expect(page).to have_content 'task'
+      end
+    end
+    context '複数のタスクを作成した場合' do
+      it 'タスクが作成日時の降順に並んでいる' do
+        new_task = FactoryBot.create(:second_task)
+        visit tasks_path
+        task_list = all('#task_row') # タスク一覧を配列として取得するため、View側でidを振っておく
+        expect(task_list[0]).to have_content 'new_task'
+        expect(task_list[1]).to have_content 'task'
       end
     end
   end
